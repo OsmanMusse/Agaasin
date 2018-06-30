547900bfe64fe548a43022b6ab047e9c2b7112fe
Osman Musse <hanad@portableuniverses-iMac.local>
2018-06-30 01:40:40 +0100

    adding new carousel to the sponsorship section

--- js/carousel.js
+++ js/carousel.js
@@ -13,11 +13,46 @@ $('.slides').slick({
       dots : true,
     },
 
-
-
   }]
-});
+}); // Closing the Slider for the carousel section
 
+// Sponsorship section Slider
+
+$('.slides-sponsor').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 860,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 660,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 
 
 
